9b675eb11f71258dec390b4cf7ed137e17925d1c
seifeslimene <s.slimene19@gmail.com>
2021-04-05 02:55:24 +0100

    Loadable Components + HMR <3

--- src/components/App.js
+++ src/components/App.js
@@ -2,30 +2,45 @@ import "./styles.scss";
 import { hot } from "react-hot-loader/root";
 import React, { Component, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// Loadable Components
+import loadable from "@loadable/component";
 // Code Splitting Using React Loadable
-import Loadable from "react-loadable";
+// import Loadable from "react-loadable";
 // Loading Component Either For Code Splitting With React Loadable Or Suspense Fallback
 import Loading from "./my-loading-component";
 // Normal Import
 // import Home from "./Home";
 // import Movies from "./Movies/Movies";
 // Using Lazy For Code Splitting
-// const Home = lazy(() => import("./Home"));
-// const Movies = lazy(() => import("./Movies/Movies"));
+const Home = lazy(() => import("./Home"));
+const Movies = lazy(() => import("./Movies/Movies"));
 
-const AsyncHome = Loadable({
-  loader: () => import("./Home"),
-  loading: Loading,
-});
-const AsyncMovies = Loadable({
-  loader: () => import("./Movies/Movies"),
-  loading: Loading,
-});
+// const AsyncHome = Loadable({
+//   loader: () => import("./Home"),
+//   loading: Loading,
+// });
+// const AsyncMovies = Loadable({
+//   loader: () => import("./Movies/Movies"),
+//   loading: Loading,
+// });
+
+// Loadable Components
+const AsyncHome = loadable(() => import("./Home"));
+const AsyncMovies = loadable(() => import("./Movies/Movies"));
+// React Loadable
+// const AsyncHome = Loadable({
+//   loader: () => import("./Home"),
+//   loading: Loading,
+// });
+// const AsyncMovies = Loadable({
+//   loader: () => import("./Movies/Movies"),
+//   loading: Loading,
+// });
 
 class App extends Component {
   render() {
     return (
-      // <Suspense fallback={<Loading />}>
+      <Suspense fallback={<Loading />}>
         <Router>
           <div>
             <nav>
@@ -52,7 +67,7 @@ class App extends Component {
             </main>
           </div>
         </Router>
-      // </Suspense>
+      </Suspense>
     );
   }
 }
